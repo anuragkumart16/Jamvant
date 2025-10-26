4ecd12d300b0518e786c945823501eb57e183f5b
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-10-26 11:13:38 +0530

    added changes

--- mobile_app/app/index.tsx
+++ mobile_app/app/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
+import { WEBVIEW_URL } from "@env";
 
 export default function App() {
+
+  const url = WEBVIEW_URL || 'http://192.168.29.80:5173/';
   return (
     <SafeAreaView style={styles.container} edges={['top', 'bottom']}>
       <WebView
-        source={{ uri: 'http://192.168.29.80:5173/' }}
+        source={{ uri: url }}
         style={{ flex: 1, backgroundColor: '#fff' }}
       />
     </SafeAreaView>
