9888b1a26f463e77be5efef8f9d42bca339032da
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-11-12 15:33:14 +0530

    added auth started working

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from "express"
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cookieParser())
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN , credentials: true }))
 
 // router imports
 import healthCheckRouter from './routes/healthCheck.js'
+import authRouter from './routes/auth.js'
 
 app.use('/healthcheck', healthCheckRouter)
+app.use('/auth', authRouter)
 
 export default app
